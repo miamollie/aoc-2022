d5f567f7bc0da479a1ed530b4967bc33813f578f
miamollie <miamollie@gmail.com>
2022-12-06 20:08:09 +1100

    Day 5 part one, moving boxes

---
+++ day5/stacks.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	stacksFilePath := os.Args[1]
+	stacksFile, err := os.Open(stacksFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stacksFile.Close()
+
+	scanner := bufio.NewScanner(stacksFile)
+	stacks := make([]string, 9)
+	for scanner.Scan() {
+		line := scanner.Text()
+		addToStacks(line, stacks)
+	}
+
+	fmt.Printf("STACKS FIRST POSITION")
+	fmt.Printf("%#v\n", stacks)
+
+	movesFilePath := os.Args[2]
+	movesFile, err := os.Open(movesFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer movesFile.Close()
+
+	scanner = bufio.NewScanner(movesFile)
+	lineCount := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineCount++
+		fmt.Println(lineCount)
+		applyMove(line, stacks)
+	}
+	fmt.Printf("STACKS FINAL POSITION")
+	fmt.Printf("%#v\n", stacks)
+}
+
+func applyMove(line string, stacks []string) {
+	sections := strings.Split(line, " ")
+	boxCount, _ := strconv.Atoi(sections[1])
+	fromColNum, _ := strconv.Atoi(sections[3])
+	fromColNum-- //array offset
+	toColNum, _ := strconv.Atoi(sections[5])
+	toColNum-- //array offset
+
+	fmt.Printf("move  %v  boxes from %v,  to %v\n", boxCount, fromColNum+1, toColNum+1)
+	for i := 0; i < boxCount; i++ {
+		fmt.Printf("%#v\n", stacks)
+		oldCol := stacks[fromColNum]
+		newCol := stacks[toColNum]
+		stacks[toColNum] = oldCol[0:1] + newCol
+		l := len(oldCol)
+		stacks[fromColNum] = oldCol[1:l]
+	}
+
+}
+
+func addToStacks(line string, stacks []string) {
+	t := strings.ReplaceAll(line, "    ", " ***")
+	fmt.Println(t)
+	re := regexp.MustCompile(`\[(.)\]|(\*\*\*)`)
+	x := re.FindAllStringSubmatch(t, -1)
+
+	for i, v := range x {
+		// fmt.Printf("stack %#v with value %#v \n", i, v[1])
+		newBlock := v[1]
+		curStack := stacks[i]
+		if newBlock != "" {
+			// fmt.Printf("stack number %v contents: %#v \n", i+1, v)
+			stacks[i] = curStack + newBlock
+		}
+	}
+}
